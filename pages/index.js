68cc26aeb70f1332271629198d04b56c3a0cd86c
Dom Vinyard <domvinyard@me.com>
2021-10-15 23:42:59 +0200

    stores

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,27 @@ import Head from 'next/head';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 
+export const AppStores = ({ width = 140, isBottom }) => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <a href={'about:blank'} target="_blank">
+        <img
+          alt="App Store"
+          style={{ width, marginRight: 8 }}
+          src="/apple_store.png"
+        />
+      </a>
+      <a href={'about:blank'} target="_blank">
+        <img
+          alt="Google Play Store"
+          style={{ width }}
+          src="/google_store.png"
+        />
+      </a>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
     <div className="container">
@@ -13,18 +34,19 @@ export default function Home() {
       <main>
         <section style={{ backgroundColor: '#222', color: '#fff' }}>
           <div className="inner">
-            <h2 className="title">
+            <h1 className="title">
               <img
                 src="/martini.svg"
                 alt="🍸"
                 style={{ width: 17, marginBottom: -3, marginRight: 4 }}
               />{' '}
               6/7
-            </h2>
+            </h1>
             <p>
               A limited alcohol lifestyle. Followers of 6/7 never consume more
               than six drinks per seven days.
             </p>
+            <AppStores />
           </div>
         </section>
         <section>
@@ -34,6 +56,7 @@ export default function Home() {
               A pint of average strength beer, a mediumish glass of wine, a
               normal sized cocktail. Don't take the piss.
             </p>
+            <AppStores />
           </div>
         </section>
       </main>
