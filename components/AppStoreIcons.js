9ab373ea9c79c2bb5fcc5c380263415528b45f12
Dom Vinyard <domvinyard@me.com>
2021-10-18 14:24:31 +0200

    link

--- components/AppStoreIcons.js
+++ components/AppStoreIcons.js
@@ -1,7 +1,9 @@
+const APP_STORE_URL = 'https://apps.apple.com/us/app/six7/id1590673634';
+
 export default function AppStoreIcons({ white = false }) {
   return (
     <div style={{ display: 'flex', flexDirection: 'row', marginTop: 30 }}>
-      <a href={'about:blank'} target="_blank">
+      <a href={APP_STORE_URL} target="_blank">
         <img
           alt="App Store"
           style={{ width: 160, marginRight: 8 }}
